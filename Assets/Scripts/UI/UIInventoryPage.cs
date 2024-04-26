60eb0b8c2b62094a8cdcb1458680e9d3f6a047bb
vin3000 <153897607+vin3000@users.noreply.github.com>
2024-04-26 16:08:19 +0200

    Gjort klart UI:n och började koda

---
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventoryPage : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryItem itemPrefab;
+
+    [SerializeField]
+    private RectTransform contentPanel;
+
+    List<UIInventoryItem> listOfItems = new List<UIInventoryItem>();
+     
+}
