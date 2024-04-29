d9fbaec28c8e18a3eb6e35904b7964eb0d4a2698
vin3000 <153897607+vin3000@users.noreply.github.com>
2024-04-29 21:38:15 +0200

    Ändrade kod för inventoryt
    Börjar med en ny modell där man använder structs för items.

--- Assets/Scripts/UI/UIInventoryPage.cs
+++ Assets/Scripts/UI/UIInventoryPage.cs
@@ -19,11 +19,13 @@ public class UIInventoryPage : MonoBehaviour
 
     List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
 
-    public Sprite image, image2;
-    public int quantity;
-    public string title, description;
-
     private int currentlyDraggedItemIndex = -1;
+
+    public event Action<int> OnDescriptionRequested,
+            OnItemActionRequested,
+            OnStartDragging;
+
+    public event Action<int, int> OnSwapItems;
 
     private void Awake()
     {
@@ -45,6 +47,14 @@ public class UIInventoryPage : MonoBehaviour
         }
     }
 
+    public void UpdateData(int itemIndex, Sprite itemImage, int itemQuantity)
+    {
+        if(listOfUIItems.Count > itemIndex)
+        {
+            listOfUIItems[itemIndex].SetData(itemImage, itemQuantity);
+        }
+    }
+
     private void HandleShowItemActions(UIInventoryItem inventoryItemUI)
     {
         
@@ -52,7 +62,7 @@ public class UIInventoryPage : MonoBehaviour
 
     private void HandleEndDrag(UIInventoryItem inventoryItemUI)
     {
-        mouseFollower.Toggle(false);
+        ResetDraggedItem();
     }
 
     private void HandleSwap(UIInventoryItem inventoryItemUI)
@@ -60,17 +70,16 @@ public class UIInventoryPage : MonoBehaviour
         int index = listOfUIItems.IndexOf(inventoryItemUI);
         if(index == -1)
         {
-            mouseFollower.Toggle(false);
-            currentlyDraggedItemIndex = -1;
             return;
         }
-        listOfUIItems[currentlyDraggedItemIndex]
-        .SetData(index == 0 ? image : image2, quantity);
-        listOfUIItems[index]
-        .SetData(currentlyDraggedItemIndex == 0 ? image : image2, quantity);
+        OnSwapItems?.Invoke(currentlyDraggedItemIndex, index);
+
+    }
+
+    private void ResetDraggedItem()
+    {
         mouseFollower.Toggle(false);
         currentlyDraggedItemIndex = -1;
-
     }
 
     private void HandleBeginDrag(UIInventoryItem inventoryItemUI)
@@ -79,27 +88,46 @@ public class UIInventoryPage : MonoBehaviour
         if(index == -1)
         return;
         currentlyDraggedItemIndex = index;
-
+        HandleItemSelection(inventoryItemUI);
+        OnStartDragging?.Invoke(index);
+    }
+    public void CreateDraggedItem(Sprite sprite, int quantity)
+    {
         mouseFollower.Toggle(true);
-        mouseFollower.SetData(index == 0 ? image : image2, quantity); 
+        mouseFollower.SetData(sprite, quantity);
     }
 
     private void HandleItemSelection(UIInventoryItem inventoryItemUI)
     {
-        itemDescription.SetDescription(image,title,description);
-        listOfUIItems[0].Select();
+        int index = listOfUIItems.IndexOf(inventoryItemUI);
+        if(index == -1)
+                return;
+        OnDescriptionRequested?.Invoke(index);
     }
 
     public void Show() {
         gameObject.SetActive(true);
-        itemDescription.ResetDescription();
-
-        listOfUIItems[0].SetData(image, quantity);
-        listOfUIItems[1].SetData(image2, quantity);
+        ResetSelection();
     }
 
-    public void Hide() {
+    private void ResetSelection()
+    {
+        itemDescription.ResetDescription();
+        DeselectAllItems();
+    }
+
+    private void DeselectAllItems()
+    {
+        foreach(UIInventoryItem item in listOfUIItems)
+        {
+            item.Deselect();
+        }
+    }
+
+    public void Hide() 
+    {
         gameObject.SetActive(false);
+        ResetDraggedItem();
     }
 
 }
