16e9b549b29e95dd92b9e1ba0a46ba2aad949ba4
vin3000 <vincent.romer@elev.ga.lbs.se>
2024-04-30 11:17:50 +0200

    La till namespaces
    Använder namespaces för min UI och inventory model för att undvika oönskade dependencies (Inventory.UI och Inventory.Model)

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -1,27 +1,82 @@
+using Inventory.Model;
+using Inventory.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryController : MonoBehaviour
+namespace Inventory
 {
-    [SerializeField]
-    private UIInventoryPage inventoryUI;
-
-    public int inventorysize = 10;
-
-    private void Start()
+    public class InventoryController : MonoBehaviour
     {
-        inventoryUI.InitializeInventoryUI(inventorysize);
-    }
+        [SerializeField]
+        private UIInventoryPage inventoryUI;
 
-    public void Update() {
-        if(Input.GetKeyDown(KeyCode.E)) {
-            if(inventoryUI.isActiveAndEnabled == false) {
-                inventoryUI.Show();
-            }
-            else
+        [SerializeField]
+        private InventorySO inventoryData;
+
+
+        private void Start()
+        {
+            PrepareUI();
+            //inventoryData.Initialize();
+        }
+
+        private void PrepareUI()
+        {
+            inventoryUI.InitializeInventoryUI(inventoryData.Size);
+            inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
+            inventoryUI.OnSwapItems += HandleSwapItems;
+            inventoryUI.OnStartDragging += HandleDragging;
+            inventoryUI.OnItemActionRequested += HandleItemActionRequest;
+        }
+
+        private void HandleItemActionRequest(int itemIndex)
+        {
+
+        }
+
+        private void HandleDragging(int itemIndex)
+        {
+
+        }
+
+        private void HandleSwapItems(int itemIndex_1, int itemIndex_2)
+        {
+
+        }
+
+        private void HandleDescriptionRequest(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.IsEmpty)
             {
-                inventoryUI.Hide();
+                inventoryUI.ResetSelection();
+                return;
+            }
+            ItemSO item = inventoryItem.item;
+            inventoryUI.UpdateDescription(itemIndex, item.ItemImage,
+                item.name, item.Description);
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (inventoryUI.isActiveAndEnabled == false)
+                {
+                    inventoryUI.Show();
+                    foreach (var item in inventoryData.GetCurrentInventoryState())
+                    {
+                        inventoryUI.UpdateData(item.Key,
+                            item.Value.item.ItemImage,
+                            item.Value.quantity);
+                    }
+                }
+                else
+                {
+                    inventoryUI.Hide();
+                }
             }
         }
     }
--- Assets/Scripts/Model/ItemSO.cs
+++ Assets/Scripts/Model/ItemSO.cs
@@ -2,27 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
-public class ItemSO : ScriptableObject
+namespace Inventory.Model
 {
-    [field: SerializeField]
-    
-    public bool IsStackable { get; set; }
+    [CreateAssetMenu]
+    public class ItemSO : ScriptableObject
+    {
+        [field: SerializeField]
 
-    public int ID => GetInstanceID();
-    
-    [field: SerializeField]
+        public bool IsStackable { get; set; }
 
-    public int MaxStackSize { get; set; } = 1;
+        public int ID => GetInstanceID();
 
-    [field: SerializeField]
-    public string Name { get; set; }
+        [field: SerializeField]
 
-    [field: SerializeField]
-    [field: TextArea]
-    public string Description { get; set; }
+        public int MaxStackSize { get; set; } = 1;
 
-    [field: SerializeField]
-    public Sprite ItemImage { get; set; }
+        [field: SerializeField]
+        public string Name { get; set; }
 
+        [field: SerializeField]
+        [field: TextArea]
+        public string Description { get; set; }
+
+        [field: SerializeField]
+        public Sprite ItemImage { get; set; }
+
+    }
 }
